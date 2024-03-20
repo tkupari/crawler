9724de44a5615b96ca48bdda5455f6cef196ea17
Teemu Kupari <tkupari@gmail.com>
2024-03-20 19:31:53 +0200

    get urls from html

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom;
+
 function normalizeURL(url) {
   const parsedUrl = new URL(url)
   let normalized = `${parsedUrl.host}${parsedUrl.pathname}`
@@ -5,9 +8,23 @@ function normalizeURL(url) {
     normalized = normalized.slice(0, -1)
   }
   return normalized
+}
 
+function getURLsFromHTML(htmlBody, baseUrl) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const links = dom.window.document.querySelectorAll('a')
+  for (const link of links) {
+    try {
+      urls.push(new URL(link.href, baseUrl).href)
+    } catch (err) {
+      console.log(`${err.message}: ${link.href}`)
+    }
+  }
+  return urls
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,20 +1,57 @@
-const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { describe, test, expect } = require('@jest/globals')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
-test('http', () => {
-  const url = "http://example.com/path"
-  const expected = "example.com/path"
-  expect(normalizeURL(url)).toEqual(expected)
+describe('normalizeURL', () => {
+  test('http', () => {
+    const url = "http://example.com/path"
+    const expected = "example.com/path"
+    expect(normalizeURL(url)).toEqual(expected)
+  })
+
+  test('https', () => {
+    const https = "https://example.com/path"
+    const expected = "example.com/path"
+    expect(normalizeURL(https)).toEqual(expected)
+  })
+
+  test('slash', () => {
+    const slash = "http://example.com/path/"
+    const expected = "example.com/path"
+    expect(normalizeURL(slash)).toEqual(expected)
+  })
 })
 
-test('https', () => {
-  const https = "https://example.com/path"
-  const expected = "example.com/path"
-  expect(normalizeURL(https)).toEqual(expected)
-})
+describe('getURLsFromHTML', () => {
+  test('absolute url', () => {
+    const baseUrl = "http://example.com"
+    const html = `<html><body><a href="http://example.com">home page</a></body></html>`
 
-test('slash', () => {
-  const slash = "http://example.com/path/"
-  const expected = "example.com/path"
-  expect(normalizeURL(slash)).toEqual(expected)
+    const expected = ["http://example.com/"]
+    expect(getURLsFromHTML(html, baseUrl)).toEqual(expected)
+  })
+
+  test('relative url', () => {
+    const baseUrl = "http://example.com"
+    const html = `<html><body><a href="/about">about page</a></body></html>`
+
+    const expected = ["http://example.com/about"]
+    expect(getURLsFromHTML(html, baseUrl)).toEqual(expected)
+  })
+
+  test('absolute and relative', () => {
+    const baseUrl = "http://example.com"
+    const html = `<html><body><a href="http:/example.com">home page</a><a href="/about">about page</a></body></html>`
+
+    const expected = ["http://example.com/", "http://example.com/about"]
+
+    expect(getURLsFromHTML(html, baseUrl)).toEqual(expected)
+  })
+
+  test('other domain', () => {
+    const baseUrl = "http://example.com"
+    const html = `<html><body><a href="http://foobar.com">home page</a></body></html>`
+
+    const expected = ["http://foobar.com/"]
+    expect(getURLsFromHTML(html, baseUrl)).toEqual(expected)
+  })
 })
